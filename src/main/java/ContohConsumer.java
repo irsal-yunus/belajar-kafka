dfc33793e7db83007fc1beed58b8f6e7bd7c6777
irsal.yunus <irsal.yunus04@gmail.com>
2021-01-10 18:10:04 +0700

    add consumer java kafka

---
+++ src/main/java/ContohConsumer.java
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ContohConsumer {
+    public static void main (String[] arg ){
+        Properties props = new Properties();
+        props.setProperty( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty( ConsumerConfig.GROUP_ID_CONFIG, "test");
+        props.setProperty( ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty( ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty( ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe( Arrays.asList("topic-java"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll( Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.println("Recieve data : " + record.value());
+        }
+    }
+}
